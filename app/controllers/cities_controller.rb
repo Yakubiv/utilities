208fdde780f6e57c57fdb6d75ff3c134379c9590
Petro <petro@MacBook-Pro-Petro.local>
2018-12-16 11:00:44 +0200

    Fix autocomplete

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,3 +1,8 @@
 class CitiesController < ApplicationController
-  autocomplete :city, :name, limit: 10 
+  def autocomplete_city_name
+    cities = City.includes(:region)
+                 .where('name LIKE ?', "%#{params[:term]}%")
+                 .order(:name).limit(10)
+    render json: cities.map { |city| { id: city.id, label: city.autocomplete_name, value: city.name } }
+  end
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -2,4 +2,8 @@ class City < ApplicationRecord
   validates :name, uniqueness: true
   belongs_to :area
   belongs_to :region
+
+  def autocomplete_name
+    "#{name} (#{region.name} район)"
+  end
 end
